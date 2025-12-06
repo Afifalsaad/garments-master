f8744a59e7d7484c2333509862198a4eca61155f
Afif Al Saad <afifalsaad.edu@gmail.com>
2025-12-07 01:03:17 +0600

    manage customer role

--- src/Pages/Dashboard/AdminDashboard/ManageUser.jsx
+++ src/Pages/Dashboard/AdminDashboard/ManageUser.jsx
@@ -1,18 +1,50 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useRef } from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const ManageUser = () => {
   const axiosSecure = useAxiosSecure();
+  const modalRef = useRef();
 
-  const { data: users = [] } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
-      console.log(res.data);
       return res.data;
     },
   });
+
+  const handleShowModal = () => {
+    modalRef.current.showModal();
+  };
+
+  const handleApprove = (user) => {
+    const updatedRole = {
+      role: "Manager",
+      status: "approved",
+    };
+    Swal.fire({
+      title: "Are you sure?",
+      text: `You want to add ${user.userName} as an manager?`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure
+          .patch(`/users/${user._id}/role`, updatedRole)
+          .then((res) => {
+            if (res.data.modifiedCount) {
+              refetch();
+              console.log("role updated");
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div>
@@ -29,6 +61,7 @@ const ManageUser = () => {
               <th>Name</th>
               <th>Email</th>
               <th>Role</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
@@ -48,11 +81,49 @@ const ManageUser = () => {
                 <td>{user.userName}</td>
                 <td>{user.userEmail}</td>
                 <td>{user.role}</td>
+                <td>
+                  {user.role === "Manager" ? (
+                    <button
+                      disabled={true}
+                      className="btn bg-[#98bbb0] hover:cursor-not-allowed text-white border-none">
+                      {" "}
+                      Approved{" "}
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handleApprove(user)}
+                      className="btn bg-[#40826D] text-white border-none">
+                      Approve
+                    </button>
+                  )}
+
+                  <button
+                    onClick={handleShowModal}
+                    className="btn bg-[#CD5C5C] text-white border-none ml-1">
+                    Reject
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
+
+      {/* Modal */}
+      <dialog ref={modalRef} className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
