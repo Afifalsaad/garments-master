d6170772ba72d4e950e11b8e85dfce6f91c452b6
Afif Al Saad <afifalsaad.edu@gmail.com>
2025-12-11 12:33:24 +0600

    implement search and filter feature

--- src/Pages/Dashboard/AdminDashboard/AllOrders.jsx
+++ src/Pages/Dashboard/AdminDashboard/AllOrders.jsx
@@ -6,15 +6,29 @@ const AllOrders = () => {
   const axiosSecure = useAxiosSecure();
   const modalRef = useRef();
   const [selectedOrder, setSelectedOrder] = useState();
+  const [searchOrder, setSearchOrder] = useState("");
+  const [filterOrder, setFilterOrder] = useState("");
 
   const { data: orders = [] } = useQuery({
     queryKey: ["orders"],
     queryFn: async () => {
       const res = await axiosSecure.get("/all-orders");
-      console.log(res.data);
       return res.data;
     },
   });
+
+  const filteredOrders = orders
+    .filter((order) => {
+      const text = searchOrder.toLowerCase();
+
+      return order.title?.toLowerCase().includes(text);
+    })
+    .filter((order) => {
+      if (filterOrder === "All" || filterOrder === "") {
+        return true;
+      }
+      return order.status === filterOrder.toLowerCase();
+    });
 
   const handleModal = (order) => {
     setSelectedOrder(order);
@@ -22,14 +36,51 @@ const AllOrders = () => {
   };
   return (
     <div>
-      {/* {loading && (
-        <div className="fixed h-screen inset-0 bg-white/50 flex items-center justify-center z-50 rounded-lg backdrop:bg-none">
-          <LoadingSpinner />
-        </div>
-      )} */}
       <h2 className="text-4xl font-bold text-center py-12">
         All Orders: {orders.length}
       </h2>
+
+      <div className="flex justify-between my-2 items-center">
+        {/* Search Box */}
+        <label className="input my-6">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24">
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor">
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input
+            onChange={(e) => setSearchOrder(e.target.value)}
+            type="search"
+            className="grow"
+            placeholder="Search Order"
+          />
+        </label>
+
+        {/* Status */}
+        <div className="w-40">
+          <fieldset className="fieldset">
+            <select
+              defaultValue={filterOrder}
+              onChange={(e) => setFilterOrder(e.target.value)}
+              className="select">
+              <option>All</option>
+              <option>Pending</option>
+              <option>Approved</option>
+              <option>Rejected</option>
+            </select>
+          </fieldset>
+        </div>
+      </div>
+
       <div className="hidden md:block">
         {/* Large Screen Table */}
         <table className="table w-full">
@@ -47,7 +98,7 @@ const AllOrders = () => {
                 </tr>
               </thead>
               <tbody>
-                {orders.map((order) => (
+                {filteredOrders.map((order) => (
                   <tr key={order._id}>
                     <td>{order._id}</td>
                     <td>
@@ -55,7 +106,7 @@ const AllOrders = () => {
                     </td>
                     <td>{order.title}</td>
                     <td>{order.order_quantity}</td>
-                    <td></td>
+                    <td>{order.status}</td>
                     <td>
                       <button
                         onClick={() => handleModal(order)}
@@ -76,12 +127,86 @@ const AllOrders = () => {
         <div className="modal-box">
           <h2 className="text-2xl font-bold text-center mb-2">Details</h2>
           {/* Table */}
-          <h2 className="font-bold">
-            Order ID:{" "}
-            <span className="text-gray-400 font-[10px]">
-              {selectedOrder?._id}
-            </span>
-          </h2>
+          <div className="overflow-x-auto">
+            <h2 className="font-bold">
+              Order ID:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?._id}
+              </span>
+            </h2>
+            <h2 className="font-bold">
+              Title:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?.title}
+              </span>
+            </h2>
+            <h2 className="font-bold">
+              Price:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?.price} BDT
+              </span>
+            </h2>
+            <h2 className="font-bold">
+              Customer Name:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?.first_name} {selectedOrder?.last_name}
+              </span>
+            </h2>
+            <h2 className="font-bold">
+              Ordered Quantity:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?.order_quantity}
+              </span>
+            </h2>
+            <h2 className="font-bold">
+              Total:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?.order_price}
+              </span>
+            </h2>
+            <h2 className="font-bold">
+              Contact No.:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?.number}
+              </span>
+            </h2>
+            <h2 className="font-bold">
+              Address:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?.address}
+              </span>
+            </h2>
+            <h2 className="font-bold">
+              Note:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?.note}
+              </span>
+            </h2>
+            <h2 className="font-bold">
+              Status:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?.status}
+              </span>
+            </h2>
+            <h2 className="font-bold">
+              Payment Option:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?.payment_option}
+              </span>
+            </h2>
+            <h2 className="font-bold">
+              Payment Status:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?.payment_status}
+              </span>
+            </h2>
+            <h2 className="font-bold">
+              Ordered At:{" "}
+              <span className="text-gray-500 font-[10px]">
+                {selectedOrder?.orderedAt}
+              </span>
+            </h2>
+          </div>
 
           <div className="modal-action">
             <form method="dialog">
@@ -89,6 +214,29 @@ const AllOrders = () => {
               <button className="btn">Close</button>
             </form>
           </div>
+        </div>
+
+        <div className="overflow-x-auto">
+          <table className="table table-zebra">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Favorite Color</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>1</th>
+                <td>Cy Ganderton</td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </dialog>
 
